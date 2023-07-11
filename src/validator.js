65e3fdbbdf8f441c56a87c89d7c5788c262966cf
Andres Bolivar <andresbolivar07@hotmail.com>
2023-07-11 17:35:07 -0500

    feat: create validate function

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,9 @@
 const validator = {
-  // ...
+  isValid(number){
+    const arrayCard = number.split("").reverse().join(", ")
+    console.log(arrayCard);
+    return true;
+  }
 };
 
 export default validator;
